beaef63796523945e7086059106dffdb3a780b91
Devraj1021 <devrajbhatade099@gmail.com>
2024-05-26 18:18:28 +0530

    initial commit

---
+++ app/main.py
@@ -0,0 +1,108 @@
+from typing import Optional
+from fastapi import Body, FastAPI, Response, status, HTTPException, Depends
+from pydantic import BaseModel
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+from sqlalchemy.orm import Session
+from .import models
+from .database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    
+while True:
+    try:
+        conn = psycopg2.connect(host = 'localhost', database = 'fastapi', user = 'postgres', password = 'raj@0324', cursor_factory = RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successful")
+        break
+    except Exception as error:
+        print("Connecting to Databases failed")
+        print("Error: ", error)
+        time.sleep(2)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return {"status": posts}
+
+@app.get("/posts")
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts = cursor.fetchall()
+    posts = db.query(models.Post).all()
+    return {"data": posts}
+
+@app.get("/posts/{post_id}")
+def get_post(post_id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts WHERE id = (%s)""", (post_id,)) # (post_id,) is used to convert the integer to tuple(str)
+    # posts = cursor.fetchone()
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id: {post_id} does not exist")
+    return {"data": post}
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(new_post : Post, db: Session = Depends(get_db)):
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", (new_post.title, new_post.content, new_post.published))
+    # posts = cursor.fetchone()
+    # conn.commit()
+    
+    post = models.Post(**new_post.model_dump())
+    # post = models.Post(title=new_post.title, content=new_post.content, published=new_post.published)
+    db.add(post)
+    db.commit()
+    db.refresh(post)
+    return {"data": post}
+
+@app.put("/posts/{id}")
+def update_post(id: int, post : Post, db: Session = Depends(get_db)):
+    # cursor.execute("""UPDATE posts SET (title, content, published) = (%s, %s, %s) WHERE id = %s RETURNING *""", (post.title, post.content, post.published, id))
+    # posts = cursor.fetchone()
+    # conn.commit()
+    
+    posts = db.query(models.Post).filter(models.Post.id == id)
+       
+    if posts.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id: {id} does not exist")
+        
+    posts.update(post.model_dump(), synchronize_session=False)
+    db.commit()
+    
+    return {"data" : posts.first()}
+
+@app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(post_id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""DELETE FROM posts WHERE id = (%s) RETURNING *""", (post_id,))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    
+    deleted_post = db.query(models.Post).filter(models.Post.id == post_id)
+    
+    if deleted_post.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id: {post_id} does not exist")
+        
+    deleted_post.delete(synchronize_session=False)
+    db.commit()
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    
+# @app.post("/createposts")
+# def create_posts(payload: dict = Body(...)):
+#     print(payload)
+#     return {"message": f"title : {payload['title']} result : {payload['result']}"}
+
