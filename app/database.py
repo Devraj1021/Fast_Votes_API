f2bf3c1af2536b0f1a82cf47361a3dd008a04a31
Devraj1021 <devrajbhatade099@gmail.com>
2024-06-09 13:02:52 +0530

    almost done

--- app/database.py
+++ app/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:abc123@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 # below one only needed if u r using SQLite
@@ -24,3 +28,14 @@ def get_db():
         yield db
     finally:
         db.close()
+        
+# while True:
+#     try:
+#         conn = psycopg2.connect(host = '', database = '', user = '', password = '', cursor_factory = RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection was successful")
+#         break
+#     except Exception as error:
+#         print("Connecting to Databases failed")
+#         print("Error: ", error)
+#         time.sleep(2)
